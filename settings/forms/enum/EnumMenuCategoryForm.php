aac475d42d52230a12c723304b07e11fcc631f17
usmonkulov <usmonkulov5@gmail.com>
2023-11-23 15:14:29 +0500

    ssuv moduli yatildi

--- settings/forms/enum/EnumMenuCategoryForm.php
+++ settings/forms/enum/EnumMenuCategoryForm.php
@@ -2,7 +2,7 @@
 
 namespace settings\forms\enum;
 
-use settings\entities\user\User;
+use settings\entities\enums\EnumMenuCategory;
 use settings\forms\enum\traits\EnumMenuCategoryTrait;
 use yii\base\Model;
 
@@ -19,10 +19,14 @@ class EnumMenuCategoryForm extends Model
             [['code_name'], 'required'],
             [['updated_by'], 'default', 'value' => null],
             [['status', 'is_deleted', 'created_by', 'updated_by'], 'integer'],
-            [['created_at', 'updated_at'], 'safe'],
             [['title_uz', 'title_oz', 'title_ru', 'title_en', 'code_name'], 'string', 'max' => 255],
-//            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
-//            [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['updated_by' => 'id']],
+            [
+                ['code_name'],
+                'unique',
+                'targetClass' => EnumMenuCategory::class,
+                'filter' =>   $this->code_name ? ['<>', 'code_name', $this->code_name] : null,
+                'targetAttribute' => ['code_name']
+            ],
         ];
     }
 }
