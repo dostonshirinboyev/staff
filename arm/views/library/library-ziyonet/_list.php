e9313ebc6d45c9cab0dbc9a122c803d75c93eee0
bobur <usmonkulov5@gmail.com>
2023-12-01 23:35:01 +0500

    arm ga envormnet to'g'irlandi 2

--- arm/views/library/library-ziyonet/_list.php
+++ arm/views/library/library-ziyonet/_list.php
@@ -12,19 +12,38 @@ use yii\helpers\Html;
         <div class="blog2-image">
             <?= Html::img($model['cover'], ['alt' => '#'])?>
             <div class="post-date">
-                June 27 <span>2021</span>
+                <?= date('d-M', strtotime($model['created_at']))?> <span><?= date('Y', strtotime($model['created_at']))?></span>
             </div>
         </div>
 
         <div class="blog2-content">
+            <h3><a href="blog-details.html"><?=substr($model['title'], 0, 80) . '...'?></a></h3>
             <ul class="blog2-meta">
-                <li><i class="far fa-folder-open"></i></li>
-                <li><a href="#">Data Strategy</a></li>
+                <li><i class="far fa-signal"></i> Daraja:</li>
+                <li><a href="#"><?=$model['level'];?></a></li>
             </ul>
-            <h3><a href="blog-details.html">The Current State Of Artificial Intelligence</a></h3>
-            <p>Integer id semper odio. Suspendisse arcu est, elementum accumsan libero vitae, lobortis dictum sem. Integer semper velit augue, at tristique ligula porta ut. </p>
+            <ul class="blog2-meta">
+                <li><i class="far fa-reply"></i> Tur:</li>
+                <li><a href="#"><?=$model['type'];?></a></li>
+            </ul>
+            <ul class="blog2-meta">
+                <li><i class="far fa-user"></i> Muallif:</li>
+                <li><a href="#"><?=$model['author'];?></a></li>
+            </ul>
+            <ul class="blog2-meta">
+                <li><i class="far fa-calendar"></i> Nashir etilgan yili:</li>
+                <li><a href="#"><?=$model['published_at'];?></a></li>
+            </ul>
+            <ul class="blog2-meta">
+                <li><i class="far fa-sort-numeric-up"></i> UDK raqami:</li>
+                <li><a href="#"><?=$model['udk'];?></a></li>
+            </ul>
+            <ul class="blog2-meta">
+                <li><i class="far fa-calendar-check"></i> Yaratilgan vaqti:</li>
+                <li><a href="#"><?=$model['created_at'];?></a></li>
+            </ul>
             <div class="blog2-button">
-                <a href="blog-details.html">Read More<i class="far fa-arrow-right"></i></a>
+                <a href="blog-details.html">Batafsil<i class="far fa-arrow-right"></i></a>
             </div>
         </div>
     </div>
