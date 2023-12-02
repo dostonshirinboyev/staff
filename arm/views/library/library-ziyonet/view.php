bc7e51a49d05df54f93e40dd7ba0a7cf63001d76
dostonshirinboyev <dshirinboyev22@gmail.com>
2023-12-02 14:54:25 +0500

    View ga shablon qo'ydim

---
+++ arm/views/library/library-ziyonet/view.php
@@ -0,0 +1,161 @@
+<section class="blog-details-area">
+    <div class="container">
+        <div class="row">
+    <div class="col-lg-9 col-12">
+        <div class="blog-details-inner">
+            <div class="blog-detail-info shadow-custom">
+                <div class="row" style="align-items: center">
+                    <div class="col-lg-4">
+                        <div class="blog-d-head-img">
+                            <div class="product-slider text-center">
+                                <a data-fancybox="product-image" data-src="/books/image/1654065050.png">
+                                    <img src="/books/image/1654065050.png" alt="Homila po'stlog'i barvaqt uzilgan homilador ayollarda homiladorlikni davom ettirishning prognozlash mezonlari">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="blog-detail-info">
+                            <div class="blog-d-tag-widget">
+                                <ul>
+                                    <li>
+                                        <a class="btn btn-block" onclick="">
+                                            <i class="fa fa-eye"></i> Online o'qish
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="" target="_blank" class="btn btn-block" onclick="">
+                                            <i class="fa fa-download"></i> Yuklab olish
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8 hr-custom">
+                        <div class="blog-d-head-title">
+                            <div class="blog-single-meta">
+                                <ul>
+                                    <li>
+                                        <h3>
+                                            Homila po'stlog'i barvaqt uzilgan homilador ayollarda homiladorlikni davom ettirishning prognozlash mezonlari
+                                        </h3>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-eye"></i><span> 1504 </span> |
+                                        <i class="fa fa-download"></i><span> 410</span>
+                                    </li>
+                                    <li>
+                                        <b>
+                                            Muallif:
+                                        </b>
+                                        Ismailova S. S.
+                                    </li>
+                                    <li>
+                                        <b>
+                                            Til:
+                                        </b>
+                                        o'zbek
+                                    </li>
+                                    <li>
+                                        <b>
+                                            Yozuv:
+                                        </b>
+                                        krill
+                                    </li>
+                                    <li>
+                                        <b>
+                                            Resurs turi:
+                                        </b>
+                                        avtoreferat
+                                    </li>
+                                    <li>
+                                        <b>
+                                            Betlar soni:
+                                        </b>
+                                        57 bet
+                                    </li>
+                                    <li>
+                                        <b>
+                                            Chop etilgan yili:
+                                        </b>
+                                        2021
+                                    </li>
+                                    <li>
+                                        <b>
+                                            Chiqish ma’lumotlari:
+                                        </b>
+                                        Toshkent
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="blog-detail-content shadow-custom">
+                <h4>
+                    Kitob haqida                      </h4>
+                <hr>
+                <p>Kiton haqida qisqacha test.</p>                    </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-12">
+        <div class="blog-detail-sidebar">
+
+            <div class="singles-s-widget socail-widget shadow-custom">
+                <h5 class="widget-title">
+                    Meni skanerlang                        </h5>
+                <img src="#" style="width: 100%; height: auto; background-size: cover;">
+
+                <h5 class="widget-title mt-4">
+                    Bizni kuzatib boring                        </h5>
+                <ul class="text-center">
+                    <li><a href="#"><i class="fab fa-telegram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                </ul>
+            </div>
+
+            <div class="singles-s-widget service-sidbar-widget service-d-menu shadow-custom">
+                <h3 class="service-d-sidebar-title">
+                    Elektron kutubxona
+                </h3>
+                <ul>
+                    <li>
+                        <a href="/uz/books/educational-literature">
+                            <i class="fas fa-circle-notch"></i>
+                            Menu 1
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/uz/books/monographs">
+                            <i class="fas fa-circle-notch"></i>
+                            Menu 2
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/uz/books/fiction">
+                            <i class="fas fa-circle-notch"></i>
+                            Menu 3
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/uz/books/methodical-instructions">
+                            <i class="fas fa-circle-notch"></i>
+                            Menu 4
+                        </a>
+                    </li>
+                    <li class="active">
+                        <a href="/uz/books/dissertations">
+                            <i class="fas fa-circle-notch"></i>
+                            Menu 5
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+</section>
