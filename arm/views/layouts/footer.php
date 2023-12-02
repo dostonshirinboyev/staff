09b0fa6621c58aafc8c10aa54891ba42d8d0fc9e
dostonshirinboyev <dshirinboyev22@gmail.com>
2023-12-02 12:01:14 +0500

    footer qismi tugallandi

--- arm/views/layouts/footer.php
+++ arm/views/layouts/footer.php
@@ -12,7 +12,7 @@
                                         <img src="/img/logo/logo.png" style="width: 60px; hwi" alt="#">
                                     </a>
                                 </div>
-                                <p>We vel ex eu diam aliquam fermentum. Maecenas leo tellus tincidunt auctor massa nec sagittis scelerisque nunc. Cras nisi tellus luctus at nisi venenatis lacinia.</p>
+                                <p>Samarqand davlat veterinariya meditsinasi, chorvachilik va bitexnologiyalar universiteti axborot resurs markazi.</p>
                                 <ul class="footer-social">
                                     <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                                     <li><a href="#"><i class="fab fa-twitter"></i></a></li>
@@ -24,62 +24,104 @@
                         <div class="col-lg-3 col-md-6 col-12">
 
                             <div class="single-widget">
-                                <h3 class="widget-title">Important Link</h3>
+                                    <h3 class="widget-title">KO'RSATGICHLAR</h3>
                                 <div class="widget-footer-info">
-                                    <a href="#"><i class="far fa-angle-double-right"></i>About Us</a>
-                                    <a href="#"><i class="far fa-angle-double-right"></i>Faq</a>
-                                    <a href="#"><i class="far fa-angle-double-right"></i>Our Blog</a>
-                                    <a href="#"><i class="far fa-angle-double-right"></i>Terms & Conditions</a>
-                                    <a href="#"><i class="far fa-angle-double-right"></i>Our Services</a>
-                                    <a href="#"><i class="far fa-angle-double-right"></i>Team Members</a>
-                                    <a href="#"><i class="far fa-angle-double-right"></i>Our Pricing</a>
-                                    <a href="#"><i class="far fa-angle-double-right"></i>Contact Us</a>
+                                    <!-- START WWW.UZ TOP-RATING -->
+                                    <SCRIPT language="javascript" type="text/javascript">
+                                        <!--
+                                        top_js="1.0";top_r="id=46446&r="+escape(document.referrer)+"&pg="+escape(window.location.href);document.cookie="smart_top=1; path=/"; top_r+="&c="+(document.cookie?"Y":"N")
+                                        //-->
+                                    </SCRIPT>
+                                    <SCRIPT language="javascript1.1" type="text/javascript">
+                                        <!--
+                                        top_js="1.1";top_r+="&j="+(navigator.javaEnabled()?"Y":"N")
+                                        //-->
+                                    </SCRIPT>
+                                    <SCRIPT language="javascript1.2" type="text/javascript">
+                                        <!--
+                                        top_js="1.2";top_r+="&wh="+screen.width+'x'+screen.height+"&px="+
+                                            (((navigator.appName.substring(0,3)=="Mic"))?screen.colorDepth:screen.pixelDepth)
+                                        //-->
+                                    </SCRIPT>
+                                    <SCRIPT language="javascript1.3" type="text/javascript">
+                                        <!--
+                                        top_js="1.3";
+                                        //-->
+                                    </SCRIPT>
+                                    <SCRIPT language="JavaScript" type="text/javascript">
+                                        <!--
+                                        top_rat="&col=342279&t=ffffff&p=4BC463";top_r+="&js="+top_js+"";document.write('<a href="http://www.uz/uz/res/visitor/index?id=46446" target=_top><img src="http://cnt0.www.uz/counter/collect?'+top_r+top_rat+'" width=88 height=31 border=0 alt="Топ рейтинг www.uz"></a>')//-->
+                                    </SCRIPT><NOSCRIPT><A href="http://www.uz/uz/res/visitor/index?id=46446" target=_top><IMG height=31 src="http://cnt0.www.uz/counter/collect?id=46446&pg=http%3A//uzinfocom.uz&&col=342279&amp;t=ffffff&amp;p=4BC463" width=88 border=0 alt="Топ рейтинг www.uz"></A></NOSCRIPT><!-- FINISH WWW.UZ TOP-RATING -->
+
+                                    <!-- Google tag (gtag.js) -->
+                                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YZK7EHCN5M"></script>
+                                    <script>
+                                        window.dataLayer = window.dataLayer || [];
+                                        function gtag(){dataLayer.push(arguments);}
+                                        gtag('js', new Date());
+
+                                        gtag('config', 'G-YZK7EHCN5M');
+                                    </script>
+
+                                    <!-- Top.Mail.Ru logo -->
+                                    <a href="https://top-fwz1.mail.ru/jump?from=3283818">
+                                        <img src="https://top-fwz1.mail.ru/counter?id=3283818;t=479;l=1" height="31" width="88" alt="Top.Mail.Ru" style="border:0;" /></a>
+                                    <!-- /Top.Mail.Ru logo -->
+
+                                    <!-- Top.Mail.Ru counter -->
+                                    <script type="text/javascript">
+                                        var _tmr = window._tmr || (window._tmr = []);
+                                        _tmr.push({id: "3283818", type: "pageView", start: (new Date()).getTime()});
+                                        (function (d, w, id) {
+                                            if (d.getElementById(id)) return;
+                                            var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+                                            ts.src = "https://top-fwz1.mail.ru/js/code.js";
+                                            var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+                                            if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+                                        })(document, window, "tmr-code");
+                                    </script>
+                                    <noscript><div><img src="https://top-fwz1.mail.ru/counter?id=3283818;js=na" style="position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div></noscript>
+                                    <!-- /Top.Mail.Ru counter -->
+
+                                    <!-- Yandex.Metrika informer -->
+<!--                                    <a href="https://metrika.yandex.ru/stat/?id=92349396&amp;from=informer" target="_blank" rel="nofollow"><img src="https://metrika-informer.com/informer/92349396/3_1_FFFFFFFF_EFEFEFFF_0_pageviews" style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="92349396" data-lang="ru" /></a> <!-- /Yandex.Metrika informer --> <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter92349396 = new Ya.Metrika({ id:92349396, clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], x = "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js", s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; for (var i = 0; i < document.scripts.length; i++) { if (document.scripts[i].src === x) { return; } } s.type = "text/javascript"; s.async = true; s.src = x; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script>-->
+                                    <!-- /Yandex.Metrika counter -->
+
+                                    <!--LiveInternet counter-->
+                                    <a href="https://www.liveinternet.ru/click"
+                                       target="_blank"><img id="licnt5F1E" width="88" height="31" style="border:0"
+                                                            title="LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня"
+                                                            src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7"
+                                                            alt=""/></a><script>(function(d,s){d.getElementById("licnt5F1E").src=
+                                            "https://counter.yadro.ru/hit?t14.6;r"+escape(d.referrer)+
+                                            ((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+
+                                                (s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+
+                                            ";h"+escape(d.title.substring(0,150))+";"+Math.random()})
+                                        (document,screen)</script>
+                                    <!--/LiveInternet-->
+
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-6 col-12">
 
                             <div class="single-widget f-contact-widget">
-                                <h3 class="widget-title">Contact Info</h3>
+                                <h3 class="widget-title">BIZNING MANZIL</h3>
                                 <ul class="f-contact-inner">
-                                    <li><i class="far fa-map-marker-alt"></i>2191 Railroad Street, <br />New York, USA</li>
-                                    <li><i class="far fa-phone"></i>+1 123 456 7894<br />+1 123 456 7895</li>
+                                    <li><i class="far fa-map-marker-alt"></i>Samarqand shahri,, <br />Mirzo Ulug'bek 77</li>
+                                    <li><i class="far fa-phone"></i>+99866 2349873<br />+99866 2349873</li>
                                     <li><i class="far fa-envelope-open"></i><a href="https://themes.lunartechit.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="adc4c3cbc2edc8d5ccc0ddc1c883cec2c0">[email&#160;protected]</a><br /><a href="https://themes.lunartechit.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="7102101d1402311409101c011d145f121e1c">[email&#160;protected]</a></li>
-                                    <li><i class="far fa-clock"></i>Fri-Mon<br />08:00pm - 06:00pm </li>
+                                    <li><i class="far fa-clock"></i>Fri-Mon<br />09:00 - 17:00 </li>
                                 </ul>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-6 col-12">
 
                             <div class="single-widget f-latest-post">
-                                <h3 class="widget-title">Latest Posts</h3>
+                                <h3 class="widget-title">BIZNING MANZIL</h3>
                                 <div class="f-latest-post-inner">
                                     <div class="single-f-post">
-                                        <div class="f-post-img">
-                                            <img src="/img/blog/4.jpg" alt="#">
-                                        </div>
-                                        <div class="f-post-content">
-                                            <h5><a href="blog-details-sidebar.html">Aenean venenatis lorem massa nec rutrum</a></h5>
-                                            <span class="f-post-date"><i class="far fa-calendar-alt"></i> June 27, 2021</span>
-                                        </div>
-                                    </div>
-                                    <div class="single-f-post">
-                                        <div class="f-post-img">
-                                            <img src="/img/blog/5.jpg" alt="#">
-                                        </div>
-                                        <div class="f-post-content">
-                                            <h5><a href="blog-details-sidebar.html">Etiam nisi odio malesuada non pretium ut</a></h5>
-                                            <span class="f-post-date"><i class="far fa-calendar-alt"></i> June 27, 2021</span>
-                                        </div>
-                                    </div>
-                                    <div class="single-f-post">
-                                        <div class="f-post-img">
-                                            <img src="/img/blog/6.jpg" alt="#">
-                                        </div>
-                                        <div class="f-post-content">
-                                            <h5><a href="blog-details-sidebar.html">Ociosqu ad litora torquent perlo conubia</a></h5>
-                                            <span class="f-post-date"><i class="far fa-calendar-alt"></i> June 27, 2021</span>
-                                        </div>
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12286.192333566758!2d66.932426!3d39.659884!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x12f6d10af242d504!2sSamarqand%20veterinariya%20meditsinasi%20instituti!5e0!3m2!1sru!2sus!4v1651235509834!5m2!1sru!2sus" width="270" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                     </div>
                                 </div>
                             </div>
@@ -97,12 +139,12 @@
                     <div class="copyright-inner">
                         <div class="row">
                             <div class="col-lg-6 col-md-6 col-12">
-                                <p class="copyright-text">&copy;Copyright 2021 <a href="#">DATAXO</a> All Rights Reserved.</p>
+                                <p class="copyright-text">&copy;Rasmiy saytimiz <a href="https://ssuv.uz">ssuv.uz</a> barcha huquqlar himoyalangan.</p>
                             </div>
                             <div class="col-lg-6 col-md-6 col-12">
                                 <div class="footer-bottom-right">
-                                    <a href="#">Support</a>
-                                    <a href="#">Privacy Policy</a>
+<!--                                    <a href="#">Support</a>-->
+<!--                                    <a href="#">Privacy Policy</a>-->
                                 </div>
                             </div>
                         </div>
