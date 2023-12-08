f923330c4864b68ad11ff8a5de2510a6c8e27164
bobur <usmonkulov5@gmail.com>
2023-12-08 09:09:20 +0500

    menyu frontenda widget orqali chiqarildi

--- settings/widgets/menu/MenuArmWidget.php
+++ settings/widgets/menu/MenuArmWidget.php
@@ -2,6 +2,7 @@
 
 namespace settings\widgets\menu;
 
+use settings\entities\enums\EnumMenuCategory;
 use settings\entities\menu\Menu;
 use settings\status\GeneralStatus;
 use yii\base\Widget;
@@ -13,9 +14,11 @@ class MenuArmWidget extends Widget
         $menuAlias = 'm';
         $menus =  Menu::find()
             ->joinWith('menus.menus')
+            ->joinWith('category')
             ->alias("{$menuAlias}")
             ->andWhere(["{$menuAlias}.parent_id" => null])
             ->andWhere(["{$menuAlias}.status" => GeneralStatus::STATUS_ENABLED])
+            ->andWhere(["enum_menu_category.code_name" => 'arm_menu'])
             ->all();
         return $this->render('menuArm', [
             'menus' => $menus
--- settings/widgets/menu/views/menuArm.php
+++ settings/widgets/menu/views/menuArm.php
@@ -1,61 +1,28 @@
 <?php
 
+use settings\entities\menu\Menu;
 
-
+/* @var $menus Menu */
 ?>
 <ul class="nav-menu mobile-menu navigation">
-    <li class="active"><a href="index-2.html">Home<i class="far fa-angle-down"></i></a>
-        <ul class="sub-menu">
-            <li class="active"><a href="index-2.html">Home One</a></li>
-            <li><a href="index-3.html">Home Two</a></li>
-            <li><a href="index-4.html">Home Three</a></li>
-        </ul>
-    </li>
-    <li><a href="about.html">About</a></li>
-    <li><a href="#">Services<i class="far fa-angle-down"></i></a>
-        <ul class="sub-menu">
-            <li><a href="service-1.html">Service One</a></li>
-            <li><a href="service-2.html">Service Two</a></li>
-            <li><a href="service-details.html">Service Details</a></li>
-        </ul>
-    </li>
-    <li><a href="#">Pages<i class="far fa-angle-down"></i></a>
-        <ul class="sub-menu">
-            <li><a href="about.html">About</a></li>
-            <li><a href="#">Team<i class="far fa-angle-right"></i></a>
-                <ul class="sub-menu-2">
-                    <li><a href="team-1.html">Team One</a></li>
-                    <li><a href="team-2.html">Team Two</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Pricing<i class="far fa-angle-right"></i></a>
-                <ul class="sub-menu-2">
-                    <li><a href="pricing-1.html">Pricing One</a></li>
-                    <li><a href="pricing-2.html">Pricing Two</a></li>
-                </ul>
-            </li>
-            <li><a href="faq.html">Faq</a></li>
-            <li><a href="portfolio.html">Project<i class="far fa-angle-right"></i></a>
-                <ul class="sub-menu-2">
-                    <li><a href="portfolio.html">Project</a></li>
-                    <li><a href="portfolio-details.html">Project Details</a></li>
-                </ul>
-            </li>
-            <li><a href="contact.html">Contact Us</a></li>
-            <li><a href="terms-conditions.html">Terms & Conditions</a></li>
-            <li><a href="privacy-policy.html">Privacy Policy</a></li>
-            <li><a href="coming-soon.html">Coming Soon</a></li>
-            <li><a href="mail-success.html">Mail Success</a></li>
-            <li><a href="404.html">404 Eror</a></li>
-        </ul>
-    </li>
-    <li><a href="blog.html">Blog<i class="far fa-angle-down"></i></a>
-        <ul class="sub-menu">
-            <li><a href="blog.html">Blog</a></li>
-            <li><a href="blog-sidebar.html">Blog Sidebar</a></li>
-            <li><a href="blog-details.html">Blog Details</a></li>
-            <li><a href="blog-details-sidebar.html">Blog Details Sidebar</a></li>
-        </ul>
-    </li>
-    <li><a href="contact.html">Contact</a></li>
+    <?php foreach ($menus as $key => $menu): ?>
+    <?php if ($menu->menus == null): ?>
+
+        <li><a href="#"><?= $menu->title_oz;?></a></li>
+
+    <?php elseif ($menu->menus != null): ?>
+
+        <li class="active"><a href="#"><?= $menu->title_oz;?><i class="far fa-angle-down"></i></a>
+            <ul class="sub-menu">
+
+                <?php foreach ($menu->menus as $subKey => $subMenu): ?>
+
+                <li class="active"><a href="#"><?= $subMenu->title_oz;?></a></li>
+
+                <?php endforeach; ?>
+            </ul>
+        </li>
+
+    <?php endif; ?>
+    <?php endforeach;?>
 </ul>
